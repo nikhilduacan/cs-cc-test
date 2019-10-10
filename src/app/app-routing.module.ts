28040fb4dbfc9b39d54f6d69136005a444d8c525
nikhilduacan <nikhilduacan@gmail.com>
2019-10-10 01:53:13 -0400

    Updating app routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+      path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
+  },
+  {
+      path: 'home', loadChildren: () => import ('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule, MatFormFieldModule, MatInputModule  } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-export const appRoutes: Routes = [
-    {
-        path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
-    },
-    {
-        path: 'home', loadChildren: () => import ('./modules/home/home.module').then(m => m.HomeModule)
-    },
-    { path: '**', redirectTo: 'login' }
-  ];
 
 @NgModule({
   declarations: [
@@ -29,8 +18,9 @@ export const appRoutes: Routes = [
     ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule, MatFormFieldModule, MatInputModule,
-    RouterModule.forRoot(appRoutes)
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
